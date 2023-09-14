d4c191e943bdb5bdb705e2c77efd16d689f964ac
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-09-14 17:15:28 -0400

    Create projects.js

---
+++ src/data/projects.js
@@ -0,0 +1,12 @@
+import TwitClone from "../images/tc-home.png";
+
+
+export const projects = [
+    {
+        name: "TwitClone",
+        desc: "Full-stack web app mimicking the basic functionality of social media platform Twitter",
+        tags: ["MongoDB", "Express", "React", "Node", "TailwindCSS"],
+        link: "https://github.com/maxhayes17/TwitterClone",
+        picture_path: TwitClone
+    },
+]
