e3c7ff318d26b7818e7f88aee66cfd64573d9108
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2024-04-19 16:39:46 -0400

    Update "download" to view

--- src/pages/index.js
+++ src/pages/index.js
@@ -155,7 +155,7 @@ const IndexPage = () => {
                     href="Software_Engineering_Infographic.pdf"
                     target="_blank"
                     className="flex flex-row bg-lavender-blush text-black w-fit space-x-4 px-5 py-3 rounded-lg hover:cursor-pointer divide-x divide-black divide-opacity-30 my-auto">
-                        <p style={{fontFamily:"'Ramabhadra', sans-serif"}}>Download</p>
+                        <p style={{fontFamily:"'Ramabhadra', sans-serif"}}>View</p>
                         <div className="pl-4">
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-black">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
@@ -177,7 +177,7 @@ const IndexPage = () => {
                     href="Pitt_Enrollment_Flowchart.pdf"
                     target="_blank"
                     className="flex flex-row bg-lavender-blush text-black w-fit space-x-4 px-5 py-3 rounded-lg hover:cursor-pointer divide-x divide-black divide-opacity-30 my-auto">
-                        <p style={{fontFamily:"'Ramabhadra', sans-serif"}}>Download</p>
+                        <p style={{fontFamily:"'Ramabhadra', sans-serif"}}>View</p>
                         <div className="pl-4">
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-black">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
