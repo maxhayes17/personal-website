d951069e1db8653d9674286cb8bbe1efbe38d618
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2024-04-19 16:24:37 -0400

    Update personal-website

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,12 +1,36 @@
-import TwitClone from "../images/tc-home.png";
+import TwitClone from "../assets/images/tc-home.png";
+import Chatbot from "../assets/images/chatbot-home.png";
+import Muve from "../assets/images/muve-page.png";
 
 
 export const projects = [
     {
+        name: "ADMT Weather Chatbot",
+        desc: "Full-stack chatbot web application which generates SQL queries from user-inputted natural language prompts, executes the queries on a weather database, and responds the results back in natural language, along with a visualization of the result if appropriate.",
+        tags: ["AI", "React", "Python", "Flask", "PostgreSQL", "Docker", "LLMs"],
+        link: "",
+        picture_path: Chatbot
+    },
+    {
         name: "TwitClone",
         desc: "Full-stack web app mimicking the basic functionality of social media platform Twitter",
-        tags: ["MongoDB", "Express", "React", "Node", "TailwindCSS"],
+        tags: ["MongoDB", "Express", "React", "Node.js", "TailwindCSS"],
         link: "https://github.com/maxhayes17/TwitterClone",
         picture_path: TwitClone
     },
+    {
+        name: "Muve",
+        desc: "Mobile application for creating, sharing, and exploring exercise routines synced with music.",
+        tags: ["UI/UX", "Flutter", "Dart", "Firebase"],
+        link: "https://github.com/maxhayes17/Muve",
+        picture_path: Muve
+    },
+    
 ]
+
+export const about = `\tHello! My name is Maximus Hayes, and I am a student at the University of Pittsburgh studying Computer Science and Data Science. 
+
+\tWith research background in Artificial Intelligence and the use of Large Language Models, most of my current work focuses on integrating Natural Language approaches to technical applications to bridge the gap between novice and expert. I also love opportunities to express a creative side to things, especially through Web Development and UI/UX design. 
+
+\tAs a Philadelphia native, I am a huge Phillies, Eagles, and 76ers fan. I also enjoy reading, specifically in the field of Psychology, and would love to talk about these things!
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import * as React from "react"
-import { motion, stagger, useAnimate } from "framer-motion"
+import { motion } from "framer-motion"
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import Project from "../components/Project";
-import { projects } from "../data/projects";
+import { projects, about } from "../data/projects";
+import SE_infographic from "../assets/images/Software_Engineering_Infographic.png";
+import Pitt_Enrollment from "../assets/images/Pitt_Enrollment_Flowchart.png";
 
 
 const pageStyles = {
@@ -12,8 +14,6 @@ const pageStyles = {
 }
 
 const IndexPage = () => {
-
-  const [scope, animate] = useAnimate()
 
   return (
     // content wrapper
@@ -35,7 +35,7 @@ const IndexPage = () => {
       >
         <div className="flex flex-row space-x-6 text-lavender-blush">
           <motion.a 
-            whileHover={{scale:1.1}} 
+            whileHover={{scale:1.05}} 
             className="my-auto hover:text-white hover:brightness-200" 
             href="https://www.github.com/maxhayes17" target="_blank" rel="noopener noreferrer">
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16" style={{margin:"auto"}}>
@@ -43,17 +43,19 @@ const IndexPage = () => {
             </svg>
           </motion.a>
           <AnchorLink href="#projects" className="my-auto font-bold hover:text-white hover:brightness-200">Projects</AnchorLink>
-          <AnchorLink href="#resume" className="my-auto font-bold hover:text-white hover:brightness-200">Resume</AnchorLink>
+          <AnchorLink href="#about" className="my-auto font-bold hover:text-white hover:brightness-200">About me</AnchorLink>
+          {/* <AnchorLink href="#resume" className="my-auto font-bold hover:text-white hover:brightness-200">Resume</AnchorLink> */}
         </div>
         <motion.a
-          whileHover={{scale:1.1}}
+          whileHover={{scale:1.05}}
           href="mailto: maxhayess17@gmail.com"
-          className="py-2 px-3 mx-8 my-auto font-bold bg-majorelle-blue rounded-lg text-lavender-blush hover:cursor-pointer"
+          className="py-2 px-3 mx-8 my-auto font-bold bg-lavender-blush rounded-lg text-eerie-black hover:cursor-pointer"
+          style={{fontFamily:"'Ramabhadra', sans-serif"}}
           >Contact me
         </motion.a>
       </motion.div>
 
-      <div className="text-center pt-32 h-screen">
+      <div className="text-center pt-64 h-screen">
         <motion.div className="inline-flex items-center"
           initial="hidden"
           whileHover="hover"
@@ -79,25 +81,6 @@ const IndexPage = () => {
           className=" text-7xl font-bold p-5" style={{fontFamily:"'Ramabhadra', sans-serif"}}>
               Maximus Hayes
           </motion.h1>
-          {/* <motion.div 
-            initial="hidden"
-            variants={{
-              hidden: {opacity: 0},
-              hover: {opacity: 1}
-            }}
-            ref={scope}
-            className="flex flex-row pt-3 w-20 justify-between items-center">
-              <motion.a className="hover:opacity-70" href="https://www.github.com/maxhayes17" target="_blank" rel="noopener noreferrer">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16" style={{margin:"auto"}}>
-                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-                </svg>
-              </motion.a>
-              <motion.a className="hover:opacity-70" href="https://www.linkedin.com/in/~max/" target="_blank" rel="noopener noreferrer">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16" style={{margin:"auto"}}>
-                  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
-                </svg>
-              </motion.a>
-          </motion.div> */}
         </motion.div>
 
           <motion.p 
@@ -114,11 +97,11 @@ const IndexPage = () => {
                 }
               }
             }}
-            className="text-md font-mono"> Student // Pittsburgh</motion.p>
+            className="text-md font-mono">Student // Pittsburgh</motion.p>
             <motion.div 
               initial="hidden"
               whileInView="visible"
-              whileHover={{scale:1.1}}
+              whileHover={{scale:1.05}}
               variants={{
                 hidden: {opacity: 0},
                 visible: {opacity: 1,
@@ -134,27 +117,6 @@ const IndexPage = () => {
               </AnchorLink>
             </motion.div>
       </div>
-      {/* <section id="about">
-        <motion.div 
-        initial="hidden"
-        whileInView="visible"
-        transition={{
-          type: "spring",
-          stiffness:50,
-          duration:0.5
-        }}
-        variants={{
-          hidden: {x:-500},
-          visible: {x:0}
-        }}
-        
-        className="w-4/5 h-screen p-5 mx-auto">
-          <div className="my-auto">
-            <h1 className="text-4xl font-bold">About me</h1>
-            <p>Lorem ipsum</p>
-          </div>
-        </motion.div>
-      </section> */}
 
       <section id="projects">
       <div className="">
@@ -164,13 +126,175 @@ const IndexPage = () => {
           
             {/* <Project name="Project1" desc="Lorem ipsum" tags={["MongoDB", "Express", "React", "Node"]} picture_path={TCHome} link="maxhayes17.github.io"/> */}
             {/* <Project name="Project2" desc="lorem ipsum" tags={["HTML"]}/> */}
+            <div className="flex flex-row w-inherit text-lavender-blush justify-around px-16">
+              <div className="flex flex-col w-1/3 p-10 h-2/3 space-y-4">
+                <h1 className="text-4xl font-bold">Other projects</h1>
+                <p>Check out some of my non-technical work</p>
+              </div>
+              <motion.div 
+              initial="hidden"
+              whileInView="visible"
+              transition={{
+                type: "spring",
+                stiffness:50,
+                duration:0.5,
+                ease:"easeIn",
+              }}
+              variants={{
+                hidden: {x:300},
+                visible: {x:0}
+              }}
+              className="flex flex-col w-2/3 p-10 space-y-10">
 
+                <div className="flex flex-row">
+                  <div className="flex flex-col w-2/3">
+                    <h1 className="text-2xl font-bold justify-center">Software Engineering Infographic</h1>
+                    <p className="mt-4">Interested in going into Software Engineering? This Infographic will introduce what to know about the field</p>
+                    <motion.a 
+                    whileHover={{scale:1.05}} 
+                    href="Software_Engineering_Infographic.pdf"
+                    target="_blank"
+                    className="flex flex-row bg-lavender-blush text-black w-fit space-x-4 px-5 py-3 rounded-lg hover:cursor-pointer divide-x divide-black divide-opacity-30 my-auto">
+                        <p style={{fontFamily:"'Ramabhadra', sans-serif"}}>Download</p>
+                        <div className="pl-4">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-black">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                          </svg>
+                        </div>
+                    </motion.a>
+                  </div>
+                  <div className="w-1/3">
+                    <img className="h-96 mx-auto" src={SE_infographic} />
+                  </div>
+                </div>
+
+                <div className="flex flex-row">
+                  <div className="flex flex-col w-1/2">
+                    <h1 className="text-2xl font-bold justify-center">The <span className="italic text-white">Perfect Pitt Enrollment</span> Flowchart</h1>
+                    <p className="mt-4 mr-4">The perfect guide to optimize enrolling in classes for Pitt students</p>
+                    <motion.a 
+                    whileHover={{scale:1.05}} 
+                    href="Pitt_Enrollment_Flowchart.pdf"
+                    target="_blank"
+                    className="flex flex-row bg-lavender-blush text-black w-fit space-x-4 px-5 py-3 rounded-lg hover:cursor-pointer divide-x divide-black divide-opacity-30 my-auto">
+                        <p style={{fontFamily:"'Ramabhadra', sans-serif"}}>Download</p>
+                        <div className="pl-4">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-black">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                          </svg>
+                        </div>
+                    </motion.a>
+                  </div>
+                  <div className="w-1/2">
+                    <img className="h-96 mx-auto" src={Pitt_Enrollment} />
+                  </div>
+                </div>
+
+              </motion.div>
+            </div>
+        </div>
+      </section>
+
+      <section id="about">
+        <div className="flex flex-row h-screen w-inherit mt-10 mx-16 pt-24 text-lavender-blush">
+          <div className="flex flex-col w-1/2 mx-16 p-10 space-y-6">
+            <h1 className="text-4xl font-bold">About me</h1>
+            <p style={{ whiteSpace: 'break-spaces' }}>{about}</p>
+            <h1 className="text-2xl font-bold justify-center">Get in touch</h1>
+            <div className="flex flex-row space-x-6">
+              <motion.a 
+                whileHover={{scale:1.05}} 
+                className="my-auto hover:text-white hover:brightness-200" 
+                href="https://www.linkedin.com/in/~max/" target="_blank" rel="noopener noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16" style={{margin:"auto"}}>
+                  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                </svg>
+              </motion.a>
+              <motion.a 
+                whileHover={{scale:1.05}} 
+                className="my-auto hover:text-white hover:brightness-200" 
+                href="https://www.github.com/maxhayes17" target="_blank" rel="noopener noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16" style={{margin:"auto"}}>
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                </svg>
+              </motion.a>
+              <motion.a 
+                whileHover={{scale:1.05}} 
+                className="my-auto hover:text-white hover:brightness-200" 
+                href="mailto: maxhayess17@gmail.com" target="_blank" rel="noopener noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-9 h-9" style={{margin:"auto"}}>
+                  <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+                  <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+                </svg>
+              </motion.a>
+            </div>
+          </div>
+
+          <div className="flex flex-col w-1/2 mx-16 p-10 space-y-6">
+            <h1 className="text-2xl font-bold justify-center">Want to learn more about me?</h1>
+            <p className="text-lavender-blush text-md pb-6">Check out these documents</p>
+
+            {/* CV btn group */}
+            <motion.a 
+            whileHover={{scale:1.05}} 
+            href="Maximus_Hayes_Resume.pdf"
+            target="_blank"
+            className="flex flex-row bg-lavender-blush text-black w-fit space-x-3 px-5 py-3 rounded-lg hover:cursor-pointer divide-x divide-black divide-opacity-30">
+              <p style={{fontFamily:"'Ramabhadra', sans-serif"}}>Resume</p>
+              <div className="pl-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-black">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                </svg>
+              </div>
+            </motion.a>
+
+            <motion.a 
+            whileHover={{scale:1.05}} 
+            href="Maximus_Hayes_Cover_Letter.pdf"
+            target="_blank"
+            className="flex flex-row bg-lavender-blush text-black w-fit space-x-3 px-5 py-3 rounded-lg hover:cursor-pointer divide-x divide-black divide-opacity-30">
+
+              <p style={{fontFamily:"'Ramabhadra', sans-serif"}}>Cover Letter</p>
+              <div className="pl-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-black">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                </svg>
+              </div>
+            </motion.a>
+
+            <motion.a 
+            whileHover={{scale:1.05}} 
+            href="Maximus_Hayes_Personal_Bibliography.pdf"
+            target="_blank"
+            className="flex flex-row bg-lavender-blush text-black w-fit space-x-4 px-5 py-3 rounded-lg hover:cursor-pointer divide-x divide-black divide-opacity-30">
+                <p style={{fontFamily:"'Ramabhadra', sans-serif"}}>Personal Bibliography</p>
+                <div className="pl-4">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-black">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                  </svg>
+                </div>
+            </motion.a>
+
+            <motion.a 
+            whileHover={{scale:1.05}} 
+            href="Maximus_Hayes_Personal_Philosophy.pdf"
+            target="_blank"
+            className="flex flex-row bg-lavender-blush text-black w-fit space-x-4 px-5 py-3 rounded-lg hover:cursor-pointer divide-x divide-black divide-opacity-30">
+                <p style={{fontFamily:"'Ramabhadra', sans-serif"}}>Personal Philosophy Statement</p>
+                <div className="pl-4">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-black">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                  </svg>
+                </div>
+            </motion.a>
+
+          </div>
         </div>
       </section>
       
-      <section id="resume" className="h-96">
+      {/* <section id="resume" className="h-96">
         <h1 className="text-center text-xl font-bold">View my resume <a href="Maximus_Hayes_Resume.pdf" target="_blank" className="text-majorelle-blue">here</a></h1>
-      </section>
+      </section> */}
 
 
 
@@ -181,4 +305,4 @@ const IndexPage = () => {
 
 export default IndexPage
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Home | Maximus Hayes</title>
